52608a88d46d84f2b82ddcac3bf0b79bd5cb31c2
Heather Kirk <hskirk@gmail.com>
2019-01-27 00:55:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,8 +30,7 @@ def monopoly_with_third_tier
   :railroads=>
   {
     :pieces=>4,
-    :names => 
-      {
+    :names  {
         :reading_railroad => {}, 
           :pennsylvania_railroad => {},
           :b_and_o_railroad => {},
